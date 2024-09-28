e897244624071590fb193a2f13b17f60459ef6e9
adi_dev_97 <93173682+AdiDEV97@users.noreply.github.com>
2024-09-28 10:49:48 +0530

    Update ApiHandler.js

--- src/features/Header/Service/ApiHandler.js
+++ src/features/Header/Service/ApiHandler.js
@@ -1,5 +1,7 @@
 import { BASE_URL, performPrivateApiRequest } from "../../../Services/ApiHandler"
 
+// Header API Handler
+
 export const getUserProfileImageApi = async (profileImageName) => {
     const response = await performPrivateApiRequest.get(`${BASE_URL}/user/profile-image/${profileImageName}`, profileImageName);
     return response;
