0f608e734f1733fb31da447dfbb287f7badf9897
adi_dev_97 <93173682+AdiDEV97@users.noreply.github.com>
2024-09-29 09:58:39 +0530

    Update ApiHandler.js

--- src/features/HomePage/Services/ApiHandler.js
+++ src/features/HomePage/Services/ApiHandler.js
@@ -1,5 +1,7 @@
 import { BASE_URL, performApiRequest } from "../../../Services/ApiHandler"
 
+// Home Component API Service
+
 export const allPostsApi = async (pageNumber, pageSize) => {
     const response = performApiRequest.get(`${BASE_URL}/api/post/allPosts?pageNumber=${pageNumber}&pageSize=${pageSize}`, null, pageNumber, pageSize);
     return response;
