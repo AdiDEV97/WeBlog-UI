2c7376a8b4f46af474a14f1124d76fe8b6ce573d
adi_dev_97 <93173682+AdiDEV97@users.noreply.github.com>
2024-10-05 16:13:40 +0530

    Update ApiHandler.js

--- src/features/Signup/Service/ApiHandler.js
+++ src/features/Signup/Service/ApiHandler.js
@@ -1,5 +1,7 @@
 import { BASE_URL, performApiRequest, performPrivateApiRequest } from "../../../Services/ApiHandler";
 
+// SignUp API Handler
+
 export const signupApi = async (user) => {
     const response = await performApiRequest.post(`${BASE_URL}/api/v1/auth/register`, user);
     return response.data;
